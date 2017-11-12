83a73e221a7890ecc5eb0f4f8131e8c4e3423a05
Beth Schofield <bethmschofield@gmail.com>
2017-11-12 22:36:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,3 +30,8 @@ function removeLastDriver() {
   const driversUpdate3 = drivers.slice(0, drivers.length-1)
   return driversUpdate3;
 }
+
+function removeFirstDriver() {
+  const driversUpdate4 = drivers.slice(1);
+  return driversUpdate4;
+}
